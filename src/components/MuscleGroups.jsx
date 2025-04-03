13cc5a6e5e9ef58e6ac891d675c19ef9ad932df6
Jostein24sfj <jostein24sfj@gmail.com>
2025-04-03 09:52:25 +0200

    changed images on program section.

--- src/components/MuscleGroups.jsx
+++ src/components/MuscleGroups.jsx
@@ -12,27 +12,43 @@ export default function MuscleGroups() {
       <div className="muscleGroupCards textShadow">
         <Link to="/muscle/chest" className="card">
           <h3>Chest</h3>
-          <img className="cardImg" src="/chestMuscles.jpg" alt="chest" />
+          <img className="cardImg" src="/chestMuscle.jpg" alt="chest" />
         </Link>
         <Link to="/muscle/back" className="card">
           <h3>Back</h3>
-          <img className="cardImg" src="/backMuscles.jpg" alt="back" />
+          <img
+            className="cardImg"
+            src="/public/theBackMusclesDiagram.jpg"
+            alt="back"
+          />
         </Link>
         <Link to="/muscle/legs" className="card">
           <h3>Legs</h3>
-          <img className="cardImg" src="/legMuscles.jpg" alt="legs" />
+          <img
+            className="cardImg"
+            src="/public/legMuscleDiagram.jpg"
+            alt="legs"
+          />
         </Link>
         <Link to="/muscle/core" className="card">
           <h3>Core</h3>
-          <img className="cardImg" src="/coreMuscles.jpg" alt="core" />
+          <img
+            className="cardImg"
+            src="/public/coreMuscleDiagram.jpg"
+            alt="core"
+          />
         </Link>
         <Link to="/muscle/arms" className="card">
           <h3>Arms</h3>
-          <img className="cardImg" src="/armMuscle.jpg" alt="arms" />
+          <img className="cardImg" src="/armMusclesDiagram.jpg" alt="arms" />
         </Link>
         <Link to="/muscle/shoulders" className="card">
           <h3>Shoulders</h3>
-          <img className="cardImg" src="/shoulderMuscles.jpg" alt="shoulders" />
+          <img
+            className="cardImg"
+            src="/public/shoulderMuscleDiagram.jpg"
+            alt="shoulders"
+          />
         </Link>
       </div>
     </>
