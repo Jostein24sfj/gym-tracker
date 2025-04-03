f63db2cf7fcf44f890529105916ab68f16b732fd
Jostein24sfj <jostein24sfj@gmail.com>
2025-04-03 10:48:46 +0200

    fix img

--- src/components/MuscleGroups.jsx
+++ src/components/MuscleGroups.jsx
@@ -12,45 +12,33 @@ export default function MuscleGroups() {
       <div className="muscleGroupCards textShadow">
         <Link to="/muscle/chest" className="card">
           <h3>Chest</h3>
-          <img className="cardImg" src="/public/chestMuscle.jpg" alt="chest" />
+          <img className="cardImg" src="/chestMuscle.jpg" alt="chest" />
         </Link>
         <Link to="/muscle/back" className="card">
           <h3>Back</h3>
           <img
             className="cardImg"
-            src="/public/theBackMusclesDiagram.jpg"
+            src="/theBackMusclesDiagram.jpg"
             alt="back"
           />
         </Link>
         <Link to="/muscle/legs" className="card">
           <h3>Legs</h3>
-          <img
-            className="cardImg"
-            src="/public/legMuscleDiagram.jpg"
-            alt="legs"
-          />
+          <img className="cardImg" src="/legMuscleDiagram.jpg" alt="legs" />
         </Link>
         <Link to="/muscle/core" className="card">
           <h3>Core</h3>
-          <img
-            className="cardImg"
-            src="/public/coreMuscleDiagram.jpg"
-            alt="core"
-          />
+          <img className="cardImg" src="/coreMuscleDiagram.jpg" alt="core" />
         </Link>
         <Link to="/muscle/arms" className="card">
           <h3>Arms</h3>
-          <img
-            className="cardImg"
-            src="/public/armMusclesDiagram.jpg"
-            alt="arms"
-          />
+          <img className="cardImg" src="/armMusclesDiagram.jpg" alt="arms" />
         </Link>
         <Link to="/muscle/shoulders" className="card">
           <h3>Shoulders</h3>
           <img
             className="cardImg"
-            src="/public/shoulderMuscleDiagram.jpg"
+            src="/shoulderMuscleDiagram.jpg"
             alt="shoulders"
           />
         </Link>
