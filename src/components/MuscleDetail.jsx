8b4b37bd6cae0b5721104ba14d3a001f0ff83a0f
Jostein24sfj <jostein24sfj@gmail.com>
2025-04-03 11:25:52 +0200

    change of css muscledetail

--- src/components/MuscleDetail.jsx
+++ src/components/MuscleDetail.jsx
@@ -17,46 +17,46 @@ export default function MuscleDetail() {
         <h2>{muscleName.toUpperCase()}</h2>
         <p>More details about {muscleName} exercises and programs.</p>
       </div>
-      <div className="øvelser">
-        <div className="øvelseInfo">
-          <img className="cardImg" src="/benchPress.png" alt="chest" />
-          <div className="øvelserInfoText">
-            <h3>Benkpress</h3>
-            <p>
-              Benkpress er en populær styrketreningsøvelse som hovedsakelig
-              trener brystmusklene (pectoralis major), men også skuldre
-              (deltoider) og triceps. Øvelsen utføres vanligvis med en vektstang
-              og løftes fra en liggende posisjon på en benk. For å utføre
-              benkpress korrekt: 1. Ligg på ryggen på en benk med føttene flatt
-              på gulvet. 2. Hold stangen med et grep som er litt bredere enn
-              skulderbredde. 3. Senk stangen kontrollert ned til brystet, og
-              trykk den deretter tilbake til utgangsposisjonen med kraft.
-              Benkpress er en av de mest brukte øvelsene for å bygge styrke og
-              masse i overkroppen. Det er også en standard øvelse i
-              styrkeløftkonkurranser.
-            </p>
+      <div className="øvelserPage">
+        <div className="øvelser">
+          <div className="øvelseInfo">
+            <img className="cardImg" src="/benchPress.png" alt="chest" />
+            <div className="øvelserInfoText">
+              <h3>Benkpress</h3>
+              <p>
+                Benkpress er en populær styrketreningsøvelse som hovedsakelig
+                trener bryst, men også skuldre og triceps. Øvelsen utføres
+                vanligvis med en vektstang og løftes fra en liggende posisjon på
+                en benk. For å utføre benkpress: 1. Ligg på ryggen på en benk
+                med føttene flatt på gulvet. 2. Hold stangen med et grep som er
+                litt bredere enn skulderbredde. 3. Senk stangen kontrollert ned
+                til brystet, og press oppover til startposisjon med kraft.
+                Benkpress er en av de mest brukte øvelsene for å bygge styrke og
+                masse i overkroppen. Det er også en standard øvelse i
+                styrkeløftkonkurranser.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="øvelser">
-        <div className="øvelseInfo">
-          <img className="cardImg" src="/cableFlyes.webp" alt="cableflyes" />
-          <div className="øvelserInfoText">
-            <h3>Cable Flyes</h3>
-            <p>
-              Cable flyes er en styrketreningsøvelse som primært trener
-              brystmusklene (pectoralis major). Øvelsen utføres ved hjelp av
-              kabelmaskiner, og gir en konstant spenning gjennom hele
-              bevegelsen, noe som gir en effektiv trening for brystet. For å
-              utføre cable flyes: 1. Juster kabelmaskinen til øyehøyde og fest
-              håndtakene på begge sidene. 2. Ta tak i håndtakene med begge
-              hender og stå i en stabil posisjon, med et lite bøy i knærne. 3.
-              Trekk hendene sammen foran kroppen, og fokuser på å klemme
-              brystmusklene på toppen. 4. Senk langsomt hendene tilbake til
-              utgangsposisjonen, og kontroller bevegelsen. Cable flyes er en
-              flott øvelse for å isolere brystmusklene og for å forbedre
-              brystets form og definisjon.
-            </p>
+        <div className="øvelser">
+          <div className="øvelseInfo">
+            <img className="cardImg" src="/cableFlyes.webp" alt="cableflyes" />
+            <div className="øvelserInfoText">
+              <h3>Cable Flyes</h3>
+              <p>
+                Cable flyes er en treningsøvelse som primært trener bryst.
+                Øvelsen utføres ved hjelp av kabelmaskiner, og gir en konstant
+                spenning gjennom hele bevegelsen, noe som gir en effektiv
+                trening for brystet. For å utføre cable flyes: 1. Juster
+                kabelmaskinen til øyehøyde og fest håndtakene på begge sidene.
+                2. Ta tak i håndtakene med begge hender og stå i en stabil
+                posisjon, med et lite bøy i knærne. 3. Trekk hendene sammen
+                foran kroppen, og fokuser på å klemme brystmusklene på toppen.
+                4. Senk langsomt hendene tilbake til startposisjonen, og
+                kontroller bevegelsen. Cable flyes er en flott øvelse for å
+                isolere brystet og for å forbedre brystets form og definisjon.
+              </p>
+            </div>
           </div>
         </div>
       </div>
