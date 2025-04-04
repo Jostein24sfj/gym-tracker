e9f88e57a809e7636a1e64638758b6810fe51649
Jostein24sfj <jostein24sfj@gmail.com>
2025-04-04 11:25:39 +0200

    added faStar and favoriteButton.jsx

--- src/components/exerciseSection/ExerciseCard.jsx
+++ src/components/exerciseSection/ExerciseCard.jsx
@@ -1,10 +1,18 @@
+import { FaRegStar } from "react-icons/fa";
 export const ExerciseCard = ({ exercise, index }) => {
   return (
     <div key={exercise.name + index} className="øvelser">
       <div className="øvelseInfo">
         <img className="cardImg" src={exercise.src} alt="chest" />
         <div className="øvelserInfoText">
-          <h3>{exercise.name}</h3>
+          <div className="favoriteSection">
+            <h3>{exercise.name}</h3>
+            <div className="favoriteButton">
+              <FaRegStar
+                style={{ color: "yellow", width: "24px", height: "24px" }}
+              />
+            </div>
+          </div>
           <p>{exercise.description}</p>
         </div>
       </div>
